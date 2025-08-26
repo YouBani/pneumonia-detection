a8985ac601ab9b778d9b389468c16e7f15e94d6a
Youness Bani <youness.bani@gmail.com>
2025-08-25 17:37:54 -0700

    chore(trainer): define __all__ for package API

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -1,3 +1,12 @@
 from .train import train
 from .loops import train_one_epoch, validate
-from .utils import _log, build_binary_metrics, select_precision, save_checkpoint
+from .utils import build_binary_metrics, select_precision, save_checkpoint
+
+__all__ = [
+    "train",
+    "train_one_epoch",
+    "validate",
+    "build_binary_metrics",
+    "select_precision",
+    "save_checkpoint",
+]
